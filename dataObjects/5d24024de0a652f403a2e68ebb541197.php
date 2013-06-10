04ac07d19c7220b84fc2483c5c6f0e0be8f247d7
1653 <1653@1653.com>
2013-06-10 15:30:10 +0000

    user:1653;

---
+++ dataObjects/5d24024de0a652f403a2e68ebb541197.php
@@ -0,0 +1,14 @@
+<?php
+ return array (
+  'name' => 'CrmEventChains',
+  'category' => 'CrmBusinessRules',
+  'parentId' => '65646a1e9ea7959d41f1480828ad5f0b',
+  'ancestorIds' => 
+  array (
+    0 => '65646a1e9ea7959d41f1480828ad5f0b',
+  ),
+  'dataIds' => 
+  array (
+  ),
+);
+?>
